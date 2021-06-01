97e5874ffa219e3cc03a91ca254974948fb89a58
Saurabh <dsaurabh0498@gmail.com>
2021-06-01 09:17:08 -0700

    3 files added

---
+++ file2.py
@@ -0,0 +1,6 @@
+a = 20
+b = 10
+c = a + b
+d = a - b
+print ("c:",c)
+print ("d:",d)
--- python2.py
+++ python2.py
@@ -3,5 +3,10 @@ b = 48929
 c = 12344
 d = a+b-c
 e = d-a+b
+f = d+e
 print ("d",d:)
 print ("e",e:)
+print ("f",f:)
+
+
+
